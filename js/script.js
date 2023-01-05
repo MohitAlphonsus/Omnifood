bb1cc4b7d20ace1800b38a76d94c78270395dcdd
MohitAlphonsus <mohitdalphonso@gmail.com>
2023-01-05 07:05:09 +0530

    implemented smooth scrolling and closing mobile nav when scrolling

--- js/script.js
+++ js/script.js
@@ -3,10 +3,39 @@ const yearEl = document.querySelector('.year');
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
-//mobile nav
+// mobile nav
 const btnNavEl = document.querySelector('.btn-mobile-nav');
 const headerEl = document.querySelector('.header');
 
 btnNavEl.addEventListener('click', function () {
 	headerEl.classList.toggle('nav-open');
+});
+
+// smooth scrolling
+const allLinks = document.querySelectorAll('a:link');
+console.log(allLinks);
+
+allLinks.forEach(function (link) {
+	link.addEventListener('click', function (e) {
+		e.preventDefault();
+
+		const href = link.getAttribute('href');
+
+		// scroll back to top
+		if (href === '#')
+			window.scrollTo({
+				top: 0,
+				behavior: 'smooth',
+			});
+
+		// scroll to section
+		if (href !== '#' && href.startsWith('#')) {
+			const sectionEl = document.querySelector(href);
+			sectionEl.scrollIntoView({ behavior: 'smooth' });
+		}
+
+		// clos mobile nav
+		if (link.classList.contains('main-nav-link'))
+			headerEl.classList.toggle('nav-open');
+	});
 });
